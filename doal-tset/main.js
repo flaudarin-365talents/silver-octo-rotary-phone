366399ff6163f3b820b012b40c1bf2f015c41009
Frédéric Laudarin <frederic.laudarin@365talents.com>
2022-02-10 10:50:50 +0100

    JSON logs + refactoring
    Changes to be committed:
    	modified:   main.js
    	new file:   utils.js

--- doal-tset/main.js
+++ doal-tset/main.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
-import process, { exit } from "process";
+import process from "process";
 import * as http from "http";
 import _yargs from "yargs";
 import { hideBin } from "yargs/helpers";
+
+import { jsonLog, sleepNow } from "./utils.js";
 
 const yargs = _yargs(hideBin(process.argv));
 
@@ -15,7 +17,8 @@ yargs
   .option("num-requests", {
     describe: "number of requests to send",
     type: "number",
-    demandOption: true,
+    default: 1,
+    demandOption: false,
   })
   .option("delay", {
     describe: "delay between two requests in ms",
@@ -24,30 +27,45 @@ yargs
     demandOption: false,
   })
   .option("host", {
-    describe: "delay between two requests in ms",
+    describe: "send HTTP to this host",
     type: "string",
     default: "localhost",
     demandOption: false,
   })
   .option("port", {
-    describe: "delay between two requests in ms",
+    describe: "send HTTP requests to this port",
     type: "number",
-    default: 9090,
+    default: 80,
+    demandOption: false,
+  })
+  .option("json", {
+    describe: "set the format of output logs to JSON",
+    type: "boolean",
+    default: false,
     demandOption: false,
   });
 
 const client = yargs.argv.client;
-const numberOfRequest = yargs.argv.numRequests;
+const numberOfRequests = yargs.argv.numRequests;
 const reqDelay = yargs.argv.delay; // ms
+const enableJsonLogs = yargs.argv.json;
 
-console.log("Number of API calls requested:", numberOfRequest);
-console.log(`Delay between API calls: ${reqDelay} ms`);
-
-const sleepNow = (delay) =>
-  new Promise((resolve) => setTimeout(resolve, delay));
-
+if (enableJsonLogs) {
+  jsonLog({
+    event: "start",
+    numberOfRequests: numberOfRequests,
+    delayBetwRequests: reqDelay,
+    units: {
+      time: "ms",
+      size: "kByte",
+    },
+  });
+} else {
+  console.log("Number of API calls requested:", numberOfRequests);
+  console.log(`Delay between API calls: ${reqDelay} ms`);
+}
 async function repeatedGreetingsLoop() {
-  for (let count = 1; count <= numberOfRequest; count++) {
+  for (let count = 1; count <= numberOfRequests; count++) {
     // Starts a timer
     const startTime = performance.now();
 
@@ -69,36 +87,71 @@ async function repeatedGreetingsLoop() {
     // Creates the HTTP request
     const req = http.request(options, (res) => {
       const status = res.statusCode;
-      console.log(`Response #${count} status ${status}`);
+      if (!enableJsonLogs) {
+        console.log(`Response #${count} status ${status}`);
+      }
+
+      // Response data chunks
       let respBodyChunks = [];
+
+      // Size of data chunks
       let respBodyChunksSizes = [];
       res
+        // On getting response body chunks
         .on("data", (chunk) => {
-          // console.log(`received: ${chunk.length} chars`);
           respBodyChunks.push(chunk);
           respBodyChunksSizes.push(chunk.length);
         })
+        // On the transaction's end
         .on("end", () => {
           const endTime = performance.now() - startTime;
-          const respBody = Buffer.concat(respBodyChunks).toString();
+          /* Concatenates the response body and converts to string
+          const respBody = Buffer.concat(respBodyChunks).toString(); */
           const respBodySize = respBodyChunksSizes.reduce((s0, s1) => s0 + s1);
-          // console.log(respBody);
-          console.log(
-            `Response #${count}: received ${
-              respBodySize / 1024
-            } kB; elapsed time ${endTime.toFixed(3)} ms`
-          );
+
+          // Writes response log to STDOUT
+          if (enableJsonLogs) {
+            jsonLog({
+              event: "response",
+              number: count,
+              status,
+              size: respBodySize / 1024,
+              time: endTime.toFixed(3),
+            });
+          } else {
+            console.log(
+              `Response #${count}: received ${
+                respBodySize / 1024
+              } kB; elapsed time ${endTime.toFixed(3)} ms`
+            );
+          }
         });
     });
 
     // Handles HTTP errors
-    req.on("error", (e) => {
-      console.error(`problem with request: ${e.message}`);
+    req.on("error", (err) => {
+      if (enableJsonLogs) {
+        jsonLog({
+          event: "error",
+          message: err.message,
+        });
+      } else {
+        console.error(`problem with request: ${err.message}`);
+      }
     });
 
-    // Send data
+    // Sends request
     req.end();
-    console.log(`Sent request #${count}`);
+
+    // Writes log 'A request was sent' to STDOUT
+    if (enableJsonLogs) {
+      jsonLog({
+        event: "sent",
+        number: count,
+      });
+    } else {
+      console.log(`Sent request #${count}`);
+    }
     if (reqDelay > 0) {
       await sleepNow(reqDelay);
     }
